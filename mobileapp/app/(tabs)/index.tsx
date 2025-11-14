55904fee906a559f02dbf92ef1458e57e905403b
thandosim <sim24001@byui.edu>
2025-11-14 12:03:45 +0200

    feat: Add Navbar component for improved navigation in TabOneScreen

--- mobileapp/app/(tabs)/index.tsx
+++ mobileapp/app/(tabs)/index.tsx
@@ -1,14 +1,20 @@
 import { StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import Navbar from '@/components/Navbar';
 
 export default function TabOneScreen() {
+
+  const navigation = useNavigation();
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Navbar navigation={navigation}/>
     </View>
   );
 }
