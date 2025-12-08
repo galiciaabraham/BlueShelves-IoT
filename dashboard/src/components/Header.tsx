cdfda80466b040e7a29ad3100ee849aeecb840f2
Julirey <132237425+Julirey@users.noreply.github.com>
2025-12-08 18:04:15 -0400

    style: Update navigation links and icons

--- dashboard/src/components/Header.tsx
+++ dashboard/src/components/Header.tsx
@@ -58,11 +58,10 @@ export default function Header() {
             <nav className={`md:flex md:items-center md:justify-between md:px-6 grow absolute md:static top-full left-0 right-0 md:border-0 border-y bg-gray-200 dark:bg-gray-900 md:bg-transparent md:dark:bg-transparent  md:p-0 ${isOpen ? 'block' : 'hidden'}`}>
                 <div className="md:space-x-4">
                     {/* Placeholder links for now */}
-                    <Link href="/dashboard" className="md:inline px-4 py-2 block hover:underline">Dashboard</Link>
+                    <Link href="/" className="md:inline px-4 py-2 block hover:underline">Dashboard</Link>
                     <Link href="/print" className="md:inline px-4 py-2 block hover:underline">Print</Link>
-                    <Link href="/settings" className="md:inline px-4 py-2 block hover:underline">Settings</Link>
                 </div>
-                <div className="space-x-4 flex items-center">
+                <div className="space-x-4 md:flex items-center">
                     
                     <button
                     onClick={handleLogout}
@@ -71,7 +70,8 @@ export default function Header() {
                     Logout
                     </button>
                     <Link href="/profile">
-                    <div className="hidden md:block w-8 h-8 bg-gray-300 rounded-full cursor-pointer hover:ring-2 hover:ring-blue-500" />
+                        <FaUserCircle className="hidden md:block text-gray-700 dark:text-gray-200" size={40} />
+                        <span className="block md:hidden px-4 py-2  hover:underline w-full text-left">Profile</span>
                     </Link>
                 </div>
             </nav>
